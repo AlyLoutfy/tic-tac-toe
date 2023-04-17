ff5f0b61320172d3dd00af477a624194bf0a18ec
AlyLoutfy <alyloutfy99@gmail.com>
2023-04-17 15:12:35 +0200

    Display message turn

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const gameBoard = (() => {
   let board = new Array(9);
 
@@ -11,7 +13,11 @@ const gameBoard = (() => {
     return board[index];
   }
 
-  return { setField, getField };
+  const reset = () => {
+    board = new Array(9);
+  }
+
+  return { setField, getField, reset };
 })();
 
 const Player = (sign) => {
@@ -25,7 +31,9 @@ const Player = (sign) => {
 };
 
 const displayController = (() => {
-  let fields = document.querySelectorAll('.field');
+  const fields = document.querySelectorAll('.field');
+  const message = document.querySelector('.message');
+  const reset = document.querySelector('.reset');
 
   fields.forEach((field) =>
     field.addEventListener('click', (e) => {
@@ -35,13 +43,32 @@ const displayController = (() => {
     })
   );
 
+  reset.addEventListener('click', (e) => {
+    gameBoard.reset();
+    gameController.reset();
+    updateGameBoard();
+    setMsg("Player O's turn");
+  })
+
   const updateGameBoard = () => {
     for (let i = 0; i < fields.length; i++) {
       fields[i].textContent = gameBoard.getField(i);
     }
   }
 
-  return { updateGameBoard };
+  const setResultMessage = (winner) => {
+    if (winner === "Draw") {
+      setMsg("It's a draw!");
+    } else {
+      setMsg(`Player ${winner} has won!`);
+    }
+  };
+
+  const setMsg = (msg) => {
+    message.textContent = msg;
+  }
+
+  return { setResultMessage, setMsg };
 })();
 
 const gameController = (() => {
@@ -52,8 +79,16 @@ const gameController = (() => {
 
   const playRound = (inputIndex) => {
     gameBoard.setField(inputIndex, getCurrentPlayerSign());
+    if (round === 9) {
+      displayController.setResultMessage("Draw");
+      gameOver = true;
+      return;
+    }
     round++;
-  }
+    displayController.setMsg(
+      `Player ${getCurrentPlayerSign()}'s turn`
+    );
+  };
 
   const getCurrentPlayerSign = () => {
     return round % 2 === 1 ? playerO.getSign() : playerX.getSign();
